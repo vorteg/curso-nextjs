d71fc10ae87ce9c5d173bccae412d9e49e040bc1
victor ortega <vorteg.r@gmail.com>
2022-09-15 22:50:55 -0500

    Respaldando como activar un tema

--- 02-pokemon-static/pages/index.tsx
+++ 02-pokemon-static/pages/index.tsx
@@ -1,9 +1,11 @@
+import { Button } from '@nextui-org/react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
     <>
       <h1>Holi!</h1>
+      <Button color="gradient">Probando</Button>
     </>
   )
 }
